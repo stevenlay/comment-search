b274556844af2e814ffbb78ff26d2ad129e84ff2
Steven Lay <stlay@ucsd.edu>
2017-10-28 21:41:02 -0700

    refactored

--- comment.py
+++ comment.py
@@ -2,16 +2,19 @@ import praw
 import sys
 import requests
 
+
 reddit = praw.Reddit("comment_searcher")
+
+#User:_____     Subreddit:_____
 user= reddit.redditor("%s" % sys.argv[1])
 subreddit = sys.argv[2]
+
 print("User: " + user.name + "  Subreddit: " + subreddit)
 for comment in user.comments.top(limit=None):
-    if comment.is_submitter and (comment.subreddit == subreddit):
-        print('* ', end="")
+    if comment.subreddit == subreddit:
+        if comment.is_submitter:
+           print('* ', end="")
         print(comment.body + '\n')
-    elif comment.subreddit == subreddit:
-        print (comment.body +  '\n')
 
     
 
